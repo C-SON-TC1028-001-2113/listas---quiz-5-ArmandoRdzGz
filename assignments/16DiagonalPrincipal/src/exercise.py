c0a8b4b111bcc15795f87f4dc1754395e1bdf28b
ArmandoRdzGz <A01254249@itesm.mx>
2021-10-01 17:41:50 +0000

    quiz5_diagonal

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,6 +1,26 @@
 
+def diagonal_en_Matriz(lista):
+    lin_diagonal = []
+    for i in range(len(lista)):
+        lin_diagonal.append(lista[i][i])
+    return(lin_diagonal)
+
 def main():
   
+    mat = []
+    renglon = []
+    reng = int(input(''))
+    col = int(input(''))
+    if reng != col:
+        print('La matriz no es cuadrada')
+    else:
+        for i in range(reng):
+            renglon = []
+            for j in range(col):
+                a = int(input())
+                renglon.append(a)
+            mat.append(renglon)
+        print(diagonal_en_Matriz(mat))
 
 if __name__=='__main__':
     main()
